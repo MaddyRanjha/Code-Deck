85b171fad294b67fb3d65bf3f61e732f142e4a48
maddyRanjha <ranjhamaddheshia01@gmail.com>
2022-11-11 14:03:23 +0530

    first commit- till 3rd class

---
+++ src/screens/Error404/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Error404() {
+  return (
+    <div>Page not found</div>
+  )
+}
+export default Error404
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import LeftComponent from './LeftComponent'
+import RightComponent from './RightComponent'
+
+const StyledHome = styled.div`
+width: 100%
+height: 100vh
+`
+
+function Home() {
+  return (
+    <StyledHome>
+      <LeftComponent />
+      <RightComponent />
+    </StyledHome>
+  )
+}
+
+export default Home
